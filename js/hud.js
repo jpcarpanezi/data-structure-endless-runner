a1a40b9001736681f953be0ef9169a27eb998acc
João Pedro Carpanezi dos Santos <jpcarpanezi@gmail.com>
2020-10-05 16:23:54 -0300

    Fixes

--- js/hud.js
+++ js/hud.js
@@ -5,3 +5,14 @@ function openModal(id){
 function closeModal(id){
 	document.getElementById(id).style.display = 'none';
 }
+
+
+function startGame(){
+	if(gameArea.started == 0){
+		gameArea.start();
+		document.getElementsByTagName('canvas').focus;
+		document.getElementById('startButton').parentNode.style.display = "none";
+		backgroundFX.play();
+		gameArea.started = 1;
+	}
+}
